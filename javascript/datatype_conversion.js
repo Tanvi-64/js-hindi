1c983bbf4b94e9fca2377eab252254154d895fa0
Tanvi-64 <tanvikumbhar6417@gmail.com>
2025-01-20 14:31:51 +0000

    containrm config done

---
+++ javascript/datatype_conversion.js
@@ -0,0 +1,34 @@
+"use strict";//treats all js as new version
+//data types
+//1.boolean
+// 2.Number
+// 3.string
+// 4.bigint
+
+// undefined
+console.log(typeof null); // null-> output= object
+let value=3
+let v=-value
+console.log(v)
+// conversions and operations
+ let s1="tanvi"
+ let s2="kumbhar"
+ let s3=s1+s2  //add to join strings
+ console.log(s3);
+  
+ console.log("1" + 2);
+ console.log(1 + "2");
+ console.log("1"+2+3);//doesn't adds last two numbers as it considers all numbers as string
+
+ console.log(1+2+"3");// initially adds numbers and then joints it with last string 
+ //comparisons of datatypes
+ console.log("2"==2);// o/p->true becoz converts string into number
+ console.log("2"===2);// o/p->false becoz also checks datatypes of both as both difeerent so false o/p
+ 
+ 
+ 
+ 
+
+ 
+
+ 
