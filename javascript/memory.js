d29cd7822664f515905ecfdc12291562db49fd20
Tanvi-64 <tanvikumbhar6417@gmail.com>
2025-01-23 05:25:04 +0000

    meomry

---
+++ javascript/memory.js
@@ -0,0 +1,21 @@
+//primitive datatypes supports stack memory
+let myemail="tanvi@64"
+let myemail2=myemail
+ myemail2="tanvi@17"
+console.log(myemail);
+console.log(myemail2);
+
+
+//non-primitive datatypes supports heap memory
+let ram={
+ email:"ram@gmail.com",
+ id:"r25"
+}
+let sham=ram
+sham.email="sham@gmail.com"
+ console.log(ram);
+ console.log(sham);
+
+
+
+
