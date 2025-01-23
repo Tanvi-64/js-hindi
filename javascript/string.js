ee8e70636ff24da9265f83c9048646fddec46b95
Tanvi-64 <tanvikumbhar6417@gmail.com>
2025-01-23 06:11:57 +0000

    strings

---
+++ javascript/string.js
@@ -0,0 +1,17 @@
+
+const name="tanvikumbharf"
+const id=64 
+//sttring interpolation
+console.log(`my name is ${name}`);
+//string mathods
+console.log(name.toUpperCase());
+console.log(name.length);
+console.log(name.__proto__);
+console.log(name.charAt(4));
+console.log(name.indexOf('k'));
+const newstring=name.substring(0,5)
+console.log(newstring);
+
+
+
+
