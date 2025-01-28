6e43859f8e5fe894309a7eff5042def616c8790a
Tanvi-64 <tanvikumbhar6417@gmail.com>
2025-01-28 13:41:20 +0000

    object

--- javascript/objects.js
+++ javascript/objects.js
@@ -3,6 +3,7 @@ const Jsuser = {
     age: 20,
     email: "tanvikumbhar64@gmail.com"
 }
-console.log(Jsuser["email"]);
 console.log(Jsuser.email);
+Object.freeze(Jsuser)
+console.log(Jsuser["email"]);
 
