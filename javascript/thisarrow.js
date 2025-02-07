530b889857758b9cb1dc737f487284a720816998
Tanvi-64 <tanvikumbhar6417@gmail.com>
2025-02-07 18:52:52 +0000

    this and arrow

--- javascript/thisarrow.js
+++ javascript/thisarrow.js
@@ -1,1 +1,11 @@
-function 
+// function chai(){
+//     let username="tanvi"
+//     console.log(this.username);
+    
+// }
+const chai= ()=>{
+    let username="tanvi"
+    console.log(username);
+}
+chai()
+th
